bb68d567fdccee0a02c6f935e5bc63fcac6207c8
wrunika <2805nika@gmail.com>
2023-02-13 19:52:02 +0300

    feat: add file with tests for dectructuring assignment for arrays

--- src/07/Destructuring.tsx
+++ src/07/Destructuring.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 export type ManType = {
     name: string
     age: number
-    lessons: {title: string}[]
+    lessons: {title: string; name?: string}[]
     address: {
         street: {
             nameStr: string
@@ -11,13 +11,41 @@ export type ManType = {
     }
 }
 type PropsType = {
-    tittle: string
+    title: string
     man: ManType
+    car: {
+        model: string
+    }
 }
+
+function useDimychState(m: string) {
+    return [m, function(){} ]
+}
+function useDimychState2(m: string) {
+    return {
+        message2: m,
+        setMessage2: function(){}
+    }
+}
+
 export const ManComponent: React.FC<PropsType> = (props) => {
+//export const ManComponent: React.FC<PropsType> = ( {title, man, ...props} ) => {
+    //const {title} = props;
+    //const {name} = props.man;
+    //const {title, man:{name}} = props
+    const {title, man, ...restProps} = props
+    //const {title, man} = props
+
+    const [message, setMessage] = useDimychState('hello')  //даём любые названия переменным
+    const {message2, setMessage2} = useDimychState2('hello')  //должны использовать названия ключей в качестве переменным
+
     return (
         <div>
-
+            <h1>{title}</h1>
+            {/*<p>{name}</p>*/}
+            <p>{man.name}</p>
+            <p>{restProps.car.model}</p>
+            {/*<p>{props.car.model}</p>*/}
         </div>
     );
 };
--- src/07/destructuring.test.ts
+++ src/07/destructuring.test.ts
@@ -6,7 +6,7 @@ beforeEach( ()=>{
     man = {
         name: 'Ron',
         age: 33,
-        lessons: [ {title: '1'}, {title: '2'} ],
+        lessons: [ {title: '1'}, {title: '2'}, {title: '3', name: 'react'} ],
         address: {
             street: {
                 nameStr: 'Mira street'
@@ -26,7 +26,30 @@ test('destructing should be correct', ()=>{
     const {nameStr} = man.address.street
 
     expect(age).toBe(33);
-    expect(lessons.length).toBe(2);
+    //xpect(lessons.length).toBe(2);
+    expect(lessons.length).toBe(3);
     expect(nameStr).toBe('Mira street');
 
+})
+
+test('array destructing should be correct', () => {
+    const lesson1 = man.lessons[0];
+    const lesson2 = man.lessons[1];
+    //const [l1, l2] = man.lessons;
+    //const [ , l2] = man.lessons;
+    const [ , , l3] = man.lessons;
+    //const [l1] = man.lessons;
+    const [l1, ...restLessons] = man.lessons;
+    const [ , l2, ...restLs] = man.lessons;
+
+    //expect(l1.title).toBe('1')
+    //expect(l2.title).toBe('2')
+    expect(l3.title).toBe('3')
+    expect(restLessons.length).toBe(2)
+
+    expect(restLs[0]).toStrictEqual({title: '3', name: 'react'})
+    
+    expect(restLs.length).toBe(1)
+    expect(lesson1.title).toBe('1')
+    expect(lesson2.title).toBe('2')
 })
